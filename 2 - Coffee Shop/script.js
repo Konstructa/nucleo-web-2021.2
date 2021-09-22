24c57a567342d1c8492cf61ea62fb4c45fc034f0
Konstructa <elenprojects7@gmail.com>
2021-09-21 22:22:36 -0300

    problema no botão de menu javascript

--- 2 - Coffee Shop/script.js
+++ 2 - Coffee Shop/script.js
@@ -1,9 +1,10 @@
 
-const btnMobile = document.getElementById('btn-Mobile');
-const btnMobileF = document.getElementById('btn-MobileF');
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-btnMobile.addEventListener('click', toggleMenu);
-btnMobileF.addEventListener('click', toggleMenu);
+ const btnMobile = document.getElementById('btn-Mobile');
+ const btnMobileF = document.getElementById('btn-MobileF');
+
+ function toggleMenu() {
+     const nav = document.getElementById('nav');
+     nav.classList.toggle('active');
+ }
+ btnMobile.addEventListener('click', toggleMenu);
+ btnMobileF.addEventListener('click', toggleMenu);
