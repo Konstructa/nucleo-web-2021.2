47f19f1f22942370c7ff8b67f4599c5a360ce2a9
Konstructa <elenprojects7@gmail.com>
2021-09-22 20:10:20 -0300

    pagina finalizada

--- 2 - Coffee Shop/script.js
+++ 2 - Coffee Shop/script.js
@@ -1,4 +1,24 @@
 
+const menu = document.getElementById('.menu');
+    const btnF = document.getElementById('btn-MobileF');
+    const btnM = document.getElementById('btn-Mobile');
+
+    function show(){
+        menu.style.display = 'flex';
+        menu.style.top = '0';
+    }
+
+    function close(){
+        menu.style.top = '-100%';
+    }
+
+    btnM.addEventListener('click', show);
+    btnF.addEventListener('click', close);
+
+
+
+
+
  const btnMobile = document.getElementById('btn-Mobile');
  const btnMobileF = document.getElementById('btn-MobileF');
 
