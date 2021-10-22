779d457be22b11ab153303cf0ae2bf019d704062
Konstructa <elenprojects7@gmail.com>
2021-10-22 18:09:06 -0300

    tudo ok

--- 6 - MovieDB/index.js
+++ 6 - MovieDB/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const fetch = require('cross-fetch'); 
-
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=';
 
 
--- 6 - MovieDB/public/info.js
+++ 6 - MovieDB/public/info.js
@@ -15,30 +15,30 @@ function addPag() {
 } 
 
 async function fetchTest(page) {
-    legend.innerHTML = "Filmes populares"
-    document.getElementById ('main').innerHTML = ""
-    const response = await fetch(`/api/${page}`);
-    const data = await response.json();
+  legend.innerHTML = "Filmes populares"
+  document.getElementById ('main').innerHTML = ""
+  const response = await fetch(`/api/${page}`);
+  const data = await response.json();
 
-     data.results.forEach((film) => {   
-      document.getElementById ('main').innerHTML += 
-      `
-      <div id = "movieCard">
-      <img src="${film.poster_path == null ? 'images/yw47098_created.jpg' : IMG_URL + film.poster_path}" alt = "Sem poster"> 
-            <div id = "movieInfo">
-              <h4>${film.title}</h4>
-              <span id = "span" class = "${checkNote(film.vote_average)}">${film.vote_average}</span>
-            </div>
-            <div id = "overview">
-              <h4 class = "${checkNote(film.vote_average)}">${film.original_title}</h4>
-              <h5>${film.release_date == '' ? 'Data não encontrada' : 
-                      ((film.release_date).substring(0, 4)) < 2021 ? `Lançamento : ${(film.release_date).substring(0, 4)}` : 
-                      `Estreia: ${(film.release_date)}`}</h5>
-              <p>${film.overview == '' ? 'Sem descrição' : film.overview}</p>
-              <p>Idioma: ${(film.original_language).toUpperCase()}</p>
-            </div>
+  data.results.forEach((film) => {   
+  document.getElementById ('main').innerHTML += 
+  `
+  <div id = "movieCard">
+  <img src="${film.poster_path == null ? 'images/yw47098_created.jpg' : IMG_URL + film.poster_path}" alt = "Sem poster"> 
+        <div id = "movieInfo">
+          <h4>${film.title}</h4>
+          <span id = "span" class = "${checkNote(film.vote_average)}">${film.vote_average}</span>
         </div>
-      ` 
+        <div id = "overview">
+          <h4 class = "${checkNote(film.vote_average)}">${film.original_title}</h4>
+          <h5>${film.release_date == '' ? 'Data não encontrada' : 
+                  ((film.release_date).substring(0, 4)) < 2021 ? `Lançamento : ${(film.release_date).substring(0, 4)}` : 
+                      `Estreia: ${(film.release_date)}`}</h5>
+          <p>${film.overview == '' ? 'Sem descrição' : film.overview}</p>
+          <p>Idioma: ${(film.original_language).toUpperCase()}</p>
+        </div>
+    </div>
+  ` 
   })  
 }
 
@@ -50,26 +50,26 @@ async function searchTest() {
   const response = await fetch(`https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${search}`);
   const data = await response.json();
 
-   data.results.forEach((film) => {   
-   main.innerHTML += 
-    `
-    <div id = "movieCard">
-    <img src="${film.poster_path == null ? 'images/yw47098_created.jpg' : IMG_URL + film.poster_path}" alt = "Sem poster"> 
-          <div id = "movieInfo">
-            <h4>${film.title}</h4>
-            <span id = "span" class = "${checkNote(film.vote_average)}">${film.vote_average}</span>
-          </div>
-          <div id = "overview">
-            <h4 class = "${checkNote(film.vote_average)}">${film.original_title}</h4>
-            <h5>${film.release_date == '' ? 'Data não encontrada' : 
-                    ((film.release_date).substring(0, 4)) < 2021 ? `Lançamento : ${(film.release_date).substring(0, 4)}` : 
+  data.results.forEach((film) => {   
+  main.innerHTML += 
+  `
+  <div id = "movieCard">
+  <img src="${film.poster_path == null ? 'images/yw47098_created.jpg' : IMG_URL + film.poster_path}" alt = "Sem poster"> 
+        <div id = "movieInfo">
+          <h4>${film.title}</h4>
+          <span id = "span" class = "${checkNote(film.vote_average)}">${film.vote_average}</span>
+        </div>
+        <div id = "overview">
+          <h4 class = "${checkNote(film.vote_average)}">${film.original_title}</h4>
+          <h5>${film.release_date == '' ? 'Data não encontrada' : 
+                  ((film.release_date).substring(0, 4)) < 2021 ? `Lançamento : ${(film.release_date).substring(0, 4)}` : 
                     `Estreia: ${(film.release_date)}`}</h5>
-            <p>${film.overview == '' ? 'Sem descrição' : film.overview}</p>
-            <p>Idioma: ${(film.original_language).toUpperCase()}</p>
-          </div>
-      </div>
-    ` 
-    main.innerHTML != "" ? `${legend.innerHTML = 'Resultados'}` : ''
+          <p>${film.overview == '' ? 'Sem descrição' : film.overview}</p>
+          <p>Idioma: ${(film.original_language).toUpperCase()}</p>
+        </div>
+    </div>
+  ` 
+  main.innerHTML != "" ? `${legend.innerHTML = 'Resultados'}` : ''
 })  
 }
 
