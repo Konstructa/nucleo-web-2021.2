71413f4c25e6ace798c38bcb7e5a5ff496e1b0ff
Konstructa <elenprojects7@gmail.com>
2021-10-28 14:47:01 -0300

    tudo funcionando, não redireciona mais

--- 7 - Interior/index.js
+++ 7 - Interior/index.js
@@ -11,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 
+
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.PASSWORD  // leave user and pass blank for now
+    pass: process.env.PASSWORD  
   }
 });
 
@@ -24,12 +25,11 @@ app.post('/register', (req, res) => {
   const email = req.body.email
   const mensage = req.body.mensage
   
-  const mailOptions = {
+  let mailOptions = {
     from: 'papaolaegonu@gmail.com',
-    to: 'lenablack87@gmail.com',
-    html: `<b>Nome: </b> ${name} </p> <br>
-          <b>Email: </b> ${email} </p><br>
-          <b>Mensagem: </b>${mensage}`,
+    to: `${email}`,
+    subject: 'Interior',
+    html: `Olá meu nome é ${name}, ${mensage}`,
   };
   
   transporter.sendMail(mailOptions, function(error, info){
@@ -39,16 +39,10 @@ app.post('/register', (req, res) => {
       console.log('Email sent: ' + info.response);
     }
   });
+
 });
 
 
-/* Campo Nome só deve aceitar letras de A-Z, maiúsculas ou minúsculas e com no mínimo 3 caracteres
-Campo Email só deve aceitar email com @ e no mínimo mínimo 5 caracteres
-Todos os campos são obrigatórios!
-Exibir mensagem de erro, quando não atender a validação
- */
-
-
 app.listen(port, () => {
-  console.log(`Iniciado em http://localhost:${PORT}`)
+  console.log(`Iniciado em http://localhost:${port}`)
 })
